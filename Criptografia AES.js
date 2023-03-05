d75411edaeb496a9b84aa8bac95029cdda3b71cb
Kauan <kauanlindobonito@gmail.com>
2023-03-05 16:54:31 -0300

    Criptografia AES

---
+++ Criptografia AES.js
@@ -0,0 +1,13 @@
+var aesjs = require('aes-js');
+var key = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 ];
+var text = 'Kauan';
+var textBytes = aesjs.utils.utf8.toBytes(text);
+var aesCtr = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(5));
+var encryptedBytes = aesCtr.encrypt(textBytes);
+var encryptedHex = aesjs.utils.hex.fromBytes(encryptedBytes);
+console.log("Texto Criptografado: " + encryptedHex);
+var encryptedBytes = aesjs.utils.hex.toBytes(encryptedHex);
+var aesCtr = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(5));
+var decryptedBytes = aesCtr.decrypt(encryptedBytes);
+var decryptedText = aesjs.utils.utf8.fromBytes(decryptedBytes);
+console.log("Texto Descriptografado: " + decryptedText);
